c5070ac102d60e12cb7848e8ea120e5d2a81e60e
Yasmeen Yousef <yasmeen.yousef05@gmail.com>
2023-05-04 14:53:21 -0400

    lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 7, "M","Red" ,"Sweet", "True" );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 5, "M","orange" , "Mischiveous", "True");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 14, "F", "Yellow" ,"Mature", "True" );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 9, "M", "Green" ,"Hard-headed", "True");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 12, "F", "Blue" ,"Nice", "True");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 16, "M", "Indigo" ,"Grumpy", "True");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 10, "F", "Violet" ,"Happy", "True");
+INSERT INTO bears (age, sex, color, temperament, alive) VALUES (8, "M", "Aurora", "N/A", "True");
